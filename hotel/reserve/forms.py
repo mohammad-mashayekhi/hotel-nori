0cc9999a91eeb2b8a1959ecebc94631e5a01fc75
yaser <ydahgan@gmail.com>
2024-07-02 22:48:23 +0330

    add overlapping check for when form is used for updating

--- hotel/reserve/forms.py
+++ hotel/reserve/forms.py
@@ -35,17 +35,23 @@ class ReservationForm(forms.ModelForm):
         }
 
     def clean(self):
-        data = self.cleaned_data
+        data = super().clean()
         data["start"] = data["start"].replace(hour=12, minute=0, second=0,tzinfo=None)
         data["end"] = data["end"].replace(hour=14, minute=0, second=0, tzinfo=None)
 
         # Check for overlapping reservations
-        overlapping_reservations = Reservation.objects.filter(
-            resource_id=data["resource"],
-            status__in=["pending_payment", "confirmed"],
-        ).exclude(Q(end__lte=data['start']) | Q(start__gte=data['end']))
+        if self.instance:
+            overlapping_reservations = Reservation.objects.filter(
+                resource_id=data["resource"],
+                status__in=["pending_payment", "confirmed"],
+            ).exclude(Q(end__lte=data['start']) | Q(start__gte=data['end']) | Q(reserve_id=self.instance.reserve_id))
+        else:
+            overlapping_reservations = Reservation.objects.filter(
+                resource_id=data["resource"],
+                status__in=["pending_payment", "confirmed"],
+            ).exclude(Q(end__lte=data['start']) | Q(start__gte=data['end']))
 
         if overlapping_reservations.exists():
-            return forms.ValidationError("sometihng went wrong")
+            raise forms.ValidationError("در این زمان از قبل رزور صورت گرفته است.")
         return data
 
