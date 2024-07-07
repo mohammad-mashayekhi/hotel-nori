0785637811494469afbc651fa76537a5c9baccf1
yaser <ydahgan@gmail.com>
2024-07-07 09:31:42 +0330

    change relationship between reservation and payment form one to many to one to one

--- hotel/zarinpal/models.py
+++ hotel/zarinpal/models.py
@@ -1,13 +1,14 @@
+
 from django.db import models
 from django.conf import settings
-#from reserve.models import Reservation
+from reserve.models import Reservation
 # Create your models here.
 
 
 class Payment(models.Model):
     card_number = models.CharField(max_length=16)
     refrence_id = models.CharField(max_length=5)
-    reservation = models.ForeignKey('reserve.Reservation', on_delete=models.CASCADE)
+    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE)
     payment_date = models.DateField(auto_now_add=True)
     cost_paid = models.DecimalField(max_digits=256, decimal_places=3)
     discount = models.DecimalField(max_digits=256, decimal_places=3)
