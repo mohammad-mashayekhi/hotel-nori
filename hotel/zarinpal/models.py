1d39377ce7223b0ee68bd4f07f7cf08f59adb160
yaser <ydahgan@gmail.com>
2024-07-07 09:11:53 +0330

    add payment model

--- hotel/zarinpal/models.py
+++ hotel/zarinpal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.conf import settings
+#from reserve.models import Reservation
 # Create your models here.
+
+
+class Payment(models.Model):
+    card_number = models.CharField(max_length=16)
+    refrence_id = models.CharField(max_length=5)
+    reservation = models.ForeignKey('reserve.Reservation', on_delete=models.CASCADE)
+    payment_date = models.DateField(auto_now_add=True)
+    cost_paid = models.DecimalField(max_digits=256, decimal_places=3)
+    discount = models.DecimalField(max_digits=256, decimal_places=3)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    paid_by_stuff = models.BooleanField(default=False)
+
