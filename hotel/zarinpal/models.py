cc683353dbbfa87b2eba8f6212641ce319f16164
mohammad mashayekhi <mamashayekhi18@gmail.com>
2024-07-23 11:18:04 +0330

    fix model migrations payment and zarinpall

--- hotel/zarinpal/models.py
+++ hotel/zarinpal/models.py
@@ -7,13 +7,14 @@ from reserve.models import Reservation
 
 class Payment(models.Model):
     card_number = models.CharField(max_length=16)
-    refrence_id = models.CharField(max_length=100)
-    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE)
+    refrence_id = models.CharField(max_length=20)
+    reservation = models.ForeignKey('reserve.Reservation', on_delete=models.CASCADE)
     payment_date = models.DateField(auto_now_add=True)
     cost_paid = models.DecimalField(max_digits=30, decimal_places=3)
-    discount = models.DecimalField(max_digits=30, decimal_places=3, default=0)
+    discount = models.DecimalField(max_digits=30, decimal_places=3, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-
+    paid_by_stuff = models.BooleanField(default=False)
+    
     @property
     def paid_by_stuff(self):
         if self.user.user_status != "verified":
