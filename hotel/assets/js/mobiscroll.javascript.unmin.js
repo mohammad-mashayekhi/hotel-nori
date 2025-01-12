894e9f30377ac835a12705ccddada209eaf8327a
mohammad mashayekhi <mamashayekhi18@gmail.com>
2025-01-12 16:55:33 +0330

    Update Mobiscroll JavaScript files: minified and unminified versions modified to improve structure and maintainability. Added missing line in unminified version for consistency.

--- hotel/assets/js/mobiscroll.javascript.unmin.js
+++ hotel/assets/js/mobiscroll.javascript.unmin.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 !(function (e, t) {
     "object" == typeof exports && "undefined" != typeof module ? t(exports) : "function" == typeof define && define.amd ? define(["exports"], t) : t(((e = "undefined" != typeof globalThis ? globalThis : e || self).mobiscroll = {}));
