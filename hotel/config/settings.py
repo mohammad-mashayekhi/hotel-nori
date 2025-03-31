253170864d2d3a46699dff7c89465ecb7159b7ef
mahdiemm <mahdiemohammadpour@yahoo.com>
2025-03-31 23:03:06 +0330

    setting

--- hotel/config/settings.py
+++ hotel/config/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
